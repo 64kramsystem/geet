8444727537cc13d579317432a99932a3bee75f27
Saverio Miroddi <saverio.pub2@gmail.com>
2017-10-10 22:07:12 +0200

    Temporarily remove long help, as it's not currently supported in simple_scripting
    This is a known bug (https://github.com/saveriomiroddi/simple_scripting/issues/14).
    
    Once the bug is fixed, the help can be restored.

--- lib/geet/helpers/configuration_helper.rb
+++ lib/geet/helpers/configuration_helper.rb
@@ -5,28 +5,6 @@ require 'simple_scripting/argv'
 module Geet
   module Helpers
     class ConfigurationHelper
-      LONG_HELP = <<~'STR'
-        Usage: github_create.rb pr <title> <description> [label1,label2]
-
-        Creates a PR from the current branch.
-
-        The labels parameter is a comma-separated list of patterns; each pattern is a case-insensitive, partial match of a label.
-        If more than one label is found for a pattern, an error is raised.
-
-        Example:
-
-            $ github_create_pr.rb 'My Title' "This is
-            a long description, but don't worry, it will be escaped.
-            Just make sure to handle the quotes properly, since it's a shell string!" legacy,swf
-
-        The above will:
-
-        - create a PR with given title/description
-        - assign the authenticated user to the PR
-        - add the "Tests: Legacy" and "Needs SWF Rebuild" labels to the PR
-        - open the PR (in the browser session)
-      STR
-
       def decode_argv
         SimpleScripting::Argv.decode(
           'pr' => [
@@ -36,7 +14,6 @@ module Geet
             'title',
             'description'
           ],
-          long_help: LONG_HELP
         )
       end
 
