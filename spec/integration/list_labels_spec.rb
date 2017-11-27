d0c038dddbc2b6ff51348b3fa89ccef69dfc569c
Saverio Miroddi <saverio.pub2@gmail.com>
2017-11-27 20:39:31 +0100

    Separated ListLabels integration test into Github context

--- spec/integration/list_labels_spec.rb
+++ spec/integration/list_labels_spec.rb
@@ -6,23 +6,25 @@ require_relative '../../lib/geet/services/list_labels'
 describe Geet::Services::ListLabels do
   let(:repository) { Geet::Git::Repository.new }
 
-  it 'should list the labels' do
-    allow(repository).to receive(:remote).with('origin').and_return('git@github.com:donaldduck/geet')
+  context 'with github.com' do
+    it 'should list the labels' do
+      allow(repository).to receive(:remote).with('origin').and_return('git@github.com:donaldduck/geet')
 
-    expected_output = <<~STR
-      - bug
-      - enhancement
-      - technical_debt
-      - top_priority
-    STR
-    expected_labels = %w[bug enhancement technical_debt top_priority]
+      expected_output = <<~STR
+        - bug
+        - enhancement
+        - technical_debt
+        - top_priority
+      STR
+      expected_labels = %w[bug enhancement technical_debt top_priority]
 
-    actual_output = StringIO.new
-    actual_labels = VCR.use_cassette("list_labels") do
-      described_class.new.execute(repository, output: actual_output)
+      actual_output = StringIO.new
+      actual_labels = VCR.use_cassette("github.com/list_labels") do
+        described_class.new.execute(repository, output: actual_output)
+      end
+
+      expect(actual_output.string).to eql(expected_output)
+      expect(actual_labels).to eql(expected_labels)
     end
-
-    expect(actual_output.string).to eql(expected_output)
-    expect(actual_labels).to eql(expected_labels)
   end
 end
