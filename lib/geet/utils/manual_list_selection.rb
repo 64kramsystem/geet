1ed019780d416428f30fe05766e03f93b71ffd6a
Saverio Miroddi <saverio.pub2@gmail.com>
2018-01-25 18:59:39 +0100

    Allow no selection on ManualListSelection

--- lib/geet/utils/manual_list_selection.rb
+++ lib/geet/utils/manual_list_selection.rb
@@ -5,6 +5,8 @@ require 'temp-fork-tp-filter'
 module Geet
   module Utils
     class ManualListSelection
+      NO_SELECTION_KEY = '(none)'
+
       PAGER_SIZE = 16
 
       # entry_type:      description of the entries type.
@@ -14,14 +16,17 @@ module Geet
       # instance_method: required when non-string objects are passed as entries; its invocation on
       #                  each object must return a string, which is used as key.
       #
-      # returns: the selected entry or array of entries.
+      # returns: the selected entry or array of entries. for single selection, if no entries are
+      #          chosen, nil is returned.
       #
       def select(entry_type, entries, selection_type, instance_method: nil)
         check_entries(entries)
 
-        entries = map_entries_to_objects(entries, instance_method) if instance_method
+        entries = create_entries_map(entries, instance_method)
 
-        show_prompt(entry_type, selection_type, entries)
+        result = show_prompt(entry_type, selection_type, entries)
+
+        result
       end
 
       private
@@ -30,9 +35,10 @@ module Geet
         raise "No #{entry_type} provided!" if entries.empty?
       end
 
-      def map_entries_to_objects(entries, instance_method)
+      def create_entries_map(entries, instance_method)
         entries.each_with_object({}) do |entry, current_map|
-          current_map[entry.send(instance_method)] = entry
+          key = instance_method ? entry.send(instance_method) : entry
+          current_map[key] = entry
         end
       end
 
@@ -41,12 +47,17 @@ module Geet
 
         case selection_type
         when :single
+          entries = add_no_selection_entry(entries)
           TTY::Prompt.new.select(prompt_title, entries, filter: true, per_page: PAGER_SIZE)
         when :multiple
           TTY::Prompt.new.multi_select(prompt_title, entries, filter: true, per_page: PAGER_SIZE)
         else
           raise "Unexpected selection type: #{selection_type.inspect}"
         end
+      end
+
+      def add_no_selection_entry(entries)
+        {NO_SELECTION_KEY => nil}.merge(entries)
       end
     end
   end
