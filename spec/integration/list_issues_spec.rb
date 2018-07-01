a8a7992cb0440c1341e8b48e47fcab2d4e0cae8d
Saverio Miroddi <saverio.pub2@gmail.com>
2018-07-01 16:19:58 +0200

    Cosmetic fix in list_issues_spec.rb

--- spec/integration/list_issues_spec.rb
+++ spec/integration/list_issues_spec.rb
@@ -46,7 +46,7 @@ describe Geet::Services::ListIssues do
         actual_output = StringIO.new
 
         service_result = VCR.use_cassette('github_com/list_issues_with_assignee') do
-          described_class.new(repository, out: actual_output).execute(assignee: 'donald-fr', )
+          described_class.new(repository, out: actual_output).execute(assignee: 'donald-fr')
         end
 
         actual_issue_numbers = service_result.map(&:number)
