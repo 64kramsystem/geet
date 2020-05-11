49d23ae277273fda4243f0a9e783d758770334bb
Saverio Miroddi <saverio.pub2@gmail.com>
2020-05-11 14:53:26 +0200

    PR merge: Delete local branch and switch to main branch, if the remote one is gone

--- lib/geet/services/merge_pr.rb
+++ lib/geet/services/merge_pr.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../helpers/services_workflow_helper'
+require_relative '../shared/branches'
 
 module Geet
   module Services
@@ -12,6 +13,7 @@ module Geet
     #
     class MergePr
       include Geet::Helpers::ServicesWorkflowHelper
+      include Geet::Shared::Branches
 
       DEFAULT_GIT_CLIENT = Geet::Utils::GitClient.new
 
@@ -33,6 +35,20 @@ module Geet
           delete_remote_branch(branch)
         end
 
+        fetch_repository
+
+        if upstream_branch_gone?
+          pr_branch = @git_client.current_branch
+
+          # The rebase could also be placed after the branch deletion. There are pros/cons;
+          # currently, it's not important.
+          #
+          checkout_branch(MAIN_BRANCH)
+          rebase
+
+          delete_local_branch(pr_branch)
+        end
+
         pr
       end
 
@@ -48,6 +64,34 @@ module Geet
         @out.puts "Deleting remote branch #{branch}..."
 
         @repository.delete_branch(branch)
+      end
+
+      def fetch_repository
+        @out.puts "Fetching repository..."
+
+        @git_client.fetch
+      end
+
+      def upstream_branch_gone?
+        @git_client.upstream_branch_gone?
+      end
+
+      def checkout_branch(branch)
+        @out.puts "Checking out #{branch}..."
+
+        @git_client.checkout(branch)
+      end
+
+      def rebase
+        @out.puts "Rebasing..."
+
+        @git_client.rebase
+      end
+
+      def delete_local_branch(branch)
+        @out.puts "Deleting local branch #{branch}..."
+
+        @git_client.delete_branch(branch)
       end
     end
   end
