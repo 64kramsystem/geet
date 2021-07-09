875e6908ecc72e4501acb5528ec5d460873143b0
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-09 10:56:40 +0200

    Add support for draft PRs creation

--- lib/geet/git/repository.rb
+++ lib/geet/git/repository.rb
@@ -74,11 +74,11 @@ module Geet
         attempt_provider_call(:Milestone, :close, number, api_interface)
       end
 
-      def create_pr(title, description, head, base)
+      def create_pr(title, description, head, base, draft)
         confirm(LOCAL_ACTION_ON_UPSTREAM_REPOSITORY_MESSAGE) if local_action_on_upstream_repository? && @warnings
         confirm(ACTION_ON_PROTECTED_REPOSITORY_MESSAGE) if action_on_protected_repository? && @warnings
 
-        attempt_provider_call(:PR, :create, title, description, head, api_interface, base)
+        attempt_provider_call(:PR, :create, title, description, head, api_interface, base, draft: draft)
       end
 
       def prs(owner: nil, head: nil, milestone: nil)
