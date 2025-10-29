caa85290808523ae521c2ac36fe727f47de33364
Saverio Miroddi <saverio.pub2@gmail.com>
2025-10-29 16:44:48 +0100

    Shared::Selection: Remove unnecessary `String#freeze` calls

--- lib/geet/shared/selection.rb
+++ lib/geet/shared/selection.rb
@@ -3,9 +3,9 @@
 module Geet
   module Shared
     module Selection
-      MANUAL_LIST_SELECTION_FLAG = '-'.freeze
+      MANUAL_LIST_SELECTION_FLAG = '-'
       # Don't select anything; return the null value.
-      SKIP_LIST_SELECTION_FLAG = ''.freeze
+      SKIP_LIST_SELECTION_FLAG = ''
 
       SELECTION_SINGLE = :single
       SELECTION_MULTIPLE = :multiple
