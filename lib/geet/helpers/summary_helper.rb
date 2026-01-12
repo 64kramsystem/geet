a2f705e1d20f91e51422a8097c311ab01dd4f122
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-12 16:53:08 +0100

    SummaryHelper#split_summary: Explicitly raise error if the title is missing

--- lib/geet/helpers/summary_helper.rb
+++ lib/geet/helpers/summary_helper.rb
@@ -11,6 +11,8 @@ module Geet
 
         title, description = summary.split(/\r|\n/, 2)
 
+        raise "Title missing" if title.nil?
+
         # The title may have a residual newline char; the description may not be present,
         # or have multiple blank lines.
         [title.strip, description.to_s.strip]
