2259b0bf0e2e61412fd1770f6c13b1c767cf3a25
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-12 16:04:20 +0100

    Add Sorbet annotations to lib/geet/shared

--- lib/geet/shared/http_error.rb
+++ lib/geet/shared/http_error.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+# typed: strict
+
 module Geet
   module Shared
     class HttpError < RuntimeError
-      # Integer.
+      extend T::Sig
+
+      sig { returns(Integer) }
       attr_reader :code
 
+      sig { params(message: String, code: T.any(Integer, String)).void }
       def initialize(message, code)
         super(message)
-        @code = code.to_i
+        @code = T.let(code.to_i, Integer)
       end
     end
   end
--- lib/geet/shared/repo_permissions.rb
+++ lib/geet/shared/repo_permissions.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
-# typed: true
+# typed: strict
 
 module Geet
   module Shared
     module RepoPermissions
+      extend T::Sig
+
       PERMISSION_ADMIN = 'admin'
       PERMISSION_WRITE = 'write'
       PERMISSION_READ = 'read'
       PERMISSION_NONE = 'none'
 
+      ALL_PERMISSIONS = T.let(T.unsafe(nil), T::Array[String]) if defined?(T::sig)
       ALL_PERMISSIONS = [
         PERMISSION_ADMIN,
         PERMISSION_WRITE,
@@ -18,8 +21,9 @@ module Geet
 
       # Not worth creating a Permission class at this stage.
       #
+      sig { params(subject_permission: String, object_permission: String).returns(T::Boolean) }
       def permission_greater_or_equal_to?(subject_permission, object_permission)
-        ALL_PERMISSIONS.index(subject_permission) <= ALL_PERMISSIONS.index(object_permission)
+        T.must(ALL_PERMISSIONS.index(subject_permission)) <= T.must(ALL_PERMISSIONS.index(object_permission))
       end
     end
   end
