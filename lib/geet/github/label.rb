8cb11c099fd29e60d12ee5dcf7ea5af707a334c1
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-15 01:52:58 +0100

    Add Sorbet annotations to lib/geet/github/label.rb

--- lib/geet/github/label.rb
+++ lib/geet/github/label.rb
@@ -1,29 +1,53 @@
 # frozen_string_literal: true
+# typed: strict
 
 module Geet
   module Github
     class Label
-      attr_reader :name, :color
+      extend T::Sig
 
+      sig { returns(String) }
+      attr_reader :name
+
+      sig { returns(String) }
+      attr_reader :color
+
+      sig {
+        params(
+          name: String,
+          color: String
+        ).void
+      }
       def initialize(name, color)
         @name = name
         @color = color
       end
 
-      # Returns a flat list of names in string form.
+      sig {
+        params(
+          api_interface: Geet::Github::ApiInterface
+        ).returns(T::Array[Geet::Github::Label])
+      }
       def self.list(api_interface)
         api_path = 'labels'
-        response = api_interface.send_request(api_path, multipage: true)
+        response = T.cast(api_interface.send_request(api_path, multipage: true), T::Array[T::Hash[String, T.untyped]])
 
         response.map do |label_entry|
-          name = label_entry.fetch('name')
-          color = label_entry.fetch('color')
+          name = T.cast(label_entry.fetch('name'), String)
+          color = T.cast(label_entry.fetch('color'), String)
 
           new(name, color)
         end
       end
 
       # See https://developer.github.com/v3/issues/labels/#create-a-label
+      sig {
+        params(
+          name: String,
+          color: String,
+          api_interface: Geet::Github::ApiInterface
+        ).returns(Geet::Github::Label)
+      }
       def self.create(name, color, api_interface)
         api_path = 'labels'
         request_data = { name:, color: }
