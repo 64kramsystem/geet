1cc90738a112efa6607b12784ecb153ec916ed85
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-15 02:21:35 +0100

    Add Sorbet annotations to lib/geet/github/issue.rb

--- lib/geet/github/issue.rb
+++ lib/geet/github/issue.rb
@@ -1,20 +1,46 @@
 # frozen_string_literal: true
+# typed: strict
 
 module Geet
   module Github
     class Issue < Geet::Github::AbstractIssue
+      extend T::Sig
+
+      sig {
+        params(
+          title: String,
+          description: String,
+          api_interface: Geet::Github::ApiInterface,
+        ).returns(Geet::Github::Issue)
+      }
       def self.create(title, description, api_interface)
         api_path = 'issues'
         request_data = { title:, body: description }
 
-        response = api_interface.send_request(api_path, data: request_data)
+        response = T.cast(
+          api_interface.send_request(api_path, data: request_data),
+          T::Hash[String, T.untyped]
+        )
 
-        issue_number, title, link = response.fetch_values('number', 'title', 'html_url')
+        issue_number = T.cast(response.fetch('number'), Integer)
+        title = T.cast(response.fetch('title'), String)
+        link = T.cast(response.fetch('html_url'), String)
 
         new(issue_number, api_interface, title, link)
       end
 
-      def self.list(api_interface, assignee: nil, milestone: nil)
+      sig {
+        params(
+          api_interface: Geet::Github::ApiInterface,
+          assignee: T.nilable(Geet::Github::User),
+          milestone: T.nilable(Geet::Github::Milestone),
+          type_filter:
+            T.nilable(
+              T.proc.params(issue_data: T::Hash[String, T.untyped]).returns(T::Boolean)
+            )
+        ).returns(T::Array[Geet::Github::AbstractIssue])
+      }
+      def self.list(api_interface, assignee: nil, milestone: nil, &type_filter)
         super do |issue_data|
           !issue_data.key?('pull_request')
         end
