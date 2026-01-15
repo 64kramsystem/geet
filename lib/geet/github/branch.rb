e50d6c2bf13b191d71a6bce42d234f39f65014d2
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-15 01:47:50 +0100

    Add Sorbet annotations to lib/geet/github/branch.rb

--- lib/geet/github/branch.rb
+++ lib/geet/github/branch.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
+# typed: strict
 
 module Geet
   module Github
     class Branch
+      extend T::Sig
+
       # See https://developer.github.com/v3/git/refs/#delete-a-reference
       #
+      sig { params(name: String, api_interface: Geet::Github::ApiInterface).void }
       def self.delete(name, api_interface)
         api_path = "git/refs/heads/#{name}"
 
