e1e75eb77dcfff7b301673f1173849e47347d27f
Saverio Miroddi <saverio.pub2@gmail.com>
2026-01-15 01:42:44 +0100

    Add Sorbet annotations to lib/geet/github/abstract_issue.rb

--- lib/geet/github/abstract_issue.rb
+++ lib/geet/github/abstract_issue.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
+# typed: strict
 
 module Geet
   module Github
     # For clarity, in this class we keep only the identical logic between the subclasses, but
     # other methods could be moved in here at some complexity cost.
     class AbstractIssue
-      attr_reader :number, :title, :link
+      extend T::Sig
 
+      sig { returns(Integer) }
+      attr_reader :number
+
+      sig { returns(String) }
+      attr_reader :title
+
+      sig { returns(String) }
+      attr_reader :link
+
+      sig { params(number: Integer, api_interface: Geet::Github::ApiInterface, title: String, link: String).void }
       def initialize(number, api_interface, title, link)
         @number = number
         @api_interface = api_interface
@@ -19,6 +30,16 @@ module Geet
       # This works both for Issues and PRs, however, when the `/pulls` API (path) is used, additional
       # information is provided (e.g. `head`).
       #
+      sig {
+        params(
+          api_interface: Geet::Github::ApiInterface,
+          milestone: T.nilable(Geet::Github::Milestone),
+          assignee: T.nilable(Geet::Github::User),
+          type_filter:
+            T.nilable(
+              T.proc.params(issue_data: T::Hash[String, T.untyped]).returns(T::Boolean)
+            )
+        ).returns(T::Array[Geet::Github::AbstractIssue]) }
       def self.list(api_interface, milestone: nil, assignee: nil, &type_filter)
         api_path = 'issues'
 
@@ -26,12 +47,12 @@ module Geet
         request_params[:milestone] = milestone.number if milestone
         request_params[:assignee] = assignee.username if assignee
 
-        response = api_interface.send_request(api_path, params: request_params, multipage: true)
+        response = T.cast(api_interface.send_request(api_path, params: request_params, multipage: true), T::Array[T::Hash[String, T.untyped]])
 
         abstract_issues_list = response.map do |issue_data|
-          number = issue_data.fetch('number')
-          title = issue_data.fetch('title')
-          link = issue_data.fetch('html_url')
+          number = T.cast(issue_data.fetch('number'), Integer)
+          title = T.cast(issue_data.fetch('title'), String)
+          link = T.cast(issue_data.fetch('html_url'), String)
 
           new(number, api_interface, title, link) if type_filter.nil? || type_filter.call(issue_data)
         end
@@ -39,9 +60,7 @@ module Geet
         abstract_issues_list.compact
       end
 
-      # params:
-      #   users:   String, or Array of strings.
-      #
+      sig { params(users: T.any(String, T::Array[String])).void }
       def assign_users(users)
         api_path = "issues/#{@number}/assignees"
         request_data = { assignees: Array(users) }
@@ -49,17 +68,16 @@ module Geet
         @api_interface.send_request(api_path, data: request_data)
       end
 
-      # labels: array of strings.
-      #
+      sig { params(labels: T::Array[String]).void }
       def add_labels(labels)
         api_path = "issues/#{@number}/labels"
-        request_data = labels
 
-        @api_interface.send_request(api_path, data: request_data)
+        @api_interface.send_request(api_path, data: labels)
       end
 
       # See https://developer.github.com/v3/issues/comments/#create-a-comment
       #
+      sig { params(comment: String).void }
       def comment(comment)
         api_path = "issues/#{@number}/comments"
         request_data = { body: comment }
@@ -69,6 +87,7 @@ module Geet
 
       # See https://developer.github.com/v3/issues/#edit-an-issue
       #
+      sig { params(milestone: Integer).void }
       def edit(milestone:)
         request_data = { milestone: milestone }
         api_path = "issues/#{@number}"
